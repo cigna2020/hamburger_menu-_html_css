dd63f63a5821a363094d94f8f18cac8df4476316
Alex <cigan@ua.fm>
2021-01-10 20:22:50 +0200

    Add some final css-styles and appropriate change in the js file

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ window.addEventListener('DOMContentLoaded', () => {
         hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
+        hamburger.classList.toggle('active');
         menu.classList.toggle('menu_active');
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
+            hamburger.classList.toggle('active');
             menu.classList.toggle('menu_active');
         })
     })
